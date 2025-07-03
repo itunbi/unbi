6a668acec0988b1318dc5209f9faa7bb1ef9a1a7
itunbi <upt.it@unbi.ac.id>
2025-07-03 08:57:13 +0700

    tambahan terbaru

--- Unbi/resources/views/index.blade.php
+++ Unbi/resources/views/index.blade.php
@@ -333,7 +333,7 @@
           <div class="card">
             <div class="card-body">
               <h3 class="card-title-1">Pengumuman</h3>
-              <a href="PENGUMUMAN/Pengumuman uas genap 2425.pdf" target="_blank" class="announcement-link"><p class="card-text">
+              <a href="Pengumuman/Pengumuman uas genap 2425.pdf" target="_blank" class="announcement-link"><p class="card-text">
                 <span>Pengumuman UAS Genap 24/25</span></p>
               </a></a>
               <a href="#" class="btn btn-custom">Lihat Lainnya</a>
